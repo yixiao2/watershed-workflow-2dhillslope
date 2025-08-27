f120d61095ed5ab74067586b449d149a2988e8b1
yixiao <yixiao20@outlook.com>
2025-08-26 23:58:09 -0700

    fix PK tree issue by pflotranate.py

--- atspflotranutils/pflotranate/pflotranate.py
+++ atspflotranutils/pflotranate/pflotranate.py
@@ -66,29 +66,82 @@ def add_pflotran_components(fout, path):
     
     with open(fout) as f:
         t = ''
+        inside_pk_tree = False
+        inside_water_balance = False
+        nesting_level = 0
+        cycledriver_content = ''  # Store content from cycledriver-pktree.xml
+
+        # Read all lines from cycledriver-pktree.xml upfront
+        with open(os.path.join(pflotran_components_dir, 'cycledriver-pktree.xml')) as g:
+            cycledriver_content = g.readlines()  # Read as a list of lines
+        
         for line in f:
-            t += line
+            line_handled_flag = False
             if 'name="PK tree"' in line:
-                with open(os.path.join(pflotran_components_dir, 'cycledriver-pktree.xml')) as g:
-                    for pktree in g:
-                        t += pktree
+                t += line  # Add the line
+                line_handled_flag = True  # Mark line as handled
+                inside_pk_tree = True
+                # Append the first part of cycledriver (Header + flow and transport)
+                for pktree_line in cycledriver_content[:-1]:  # Add flow and transport, but exclude last line of "</ParameterList>"
+                    t += pktree_line
+
+            if inside_pk_tree:
+                # Detect start of "water_balance"
+                if '<ParameterList name="water_balance"' in line:
+                    #t += ''.join('  ' + line)  # Add the line
+                    #line_handled_flag = True  # Mark line as handled
+                    inside_water_balance = True
+                    nesting_level = 1  # Initialize the nesting level for this block
+                    #continue
+                
+                # Inside "water_balance", accumulate the entire block
+                if inside_water_balance:
+                    # Update nesting level for nested ParameterList tags
+                    if '<ParameterList' in line:
+                        nesting_level += 1
+                    if '</ParameterList>' in line:
+                        nesting_level -= 1
+                    # When exiting the outermost "water_balance" block
+                    if nesting_level == 0:
+                        inside_water_balance = False
+                        inside_pk_tree = False # Exit "PK tree" section
+                        t += ''.join('  ' + line)  # Add the line
+                        line_handled_flag = True  # Mark line as handled
+                        t += '</ParameterList>\n' # Add back "</ParameterList>" excluded above
+                    else:
+                        t += ''.join('  ' + line)  # Add the line
+                        line_handled_flag = True  # Mark line as handled
+                    continue
+
             if 'name="PKs"' in line:
+                t += line  # Add the line
+                line_handled_flag = True  # Mark line as handled
                 with open(os.path.join(pflotran_components_dir, 'pks.xml')) as g:
                     for pks in g:
                         t += pks
             if 'name="evaluators"' in line:
+                t += line  # Add the line
+                line_handled_flag = True  # Mark line as handled
                 with open(os.path.join(pflotran_components_dir, 'state-evals.xml')) as g:
                     for fieldeval in g:
                         t += fieldeval
             if 'name="initial conditions"' in line:
+                t += line  # Add the line
+                line_handled_flag = True  # Mark line as handled
                 with open(os.path.join(pflotran_components_dir, 'state-ics.xml')) as g:
                     for ics in g:
                         t += ics
             if 'name="observations"' in line:
+                t += line  # Add the line
+                line_handled_flag = True  # Mark line as handled
                 with open(os.path.join(pflotran_components_dir, 'obs.xml')) as g:
                     for obs in g:
                         t += obs
-    
+            
+            # Add any unhandled lines to `t` by default
+            if not line_handled_flag:
+                t += line
+                
     with open(fout, 'w') as f:
         f.write(t)
     print(f"{fout} generated (2/3)")
